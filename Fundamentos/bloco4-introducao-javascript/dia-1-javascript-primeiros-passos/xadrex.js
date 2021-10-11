76d1654157adbb4ca938963f5649cff922d2a79f
Matheus-Ferreira <m.ferreira.araujo2016@gmail.com>
2021-10-11 16:53:32 -0300

    exercicio de xadrex

---
+++ Fundamentos/bloco4-introducao-javascript/dia-1-javascript-primeiros-passos/xadrex.js
@@ -0,0 +1,25 @@
+const peca = "cavalo";
+
+switch (peca) {
+    case "rei":
+        console.log("Move-se para qualquer casa adjacente");
+        break;
+    case "rainha":
+        console.log("Move-se verticalmente, horizontalmente ou diagonalmente");
+        break;
+    case "bispo":
+        console.log("Move-se para a diagonal");
+        break;
+    case "torre":
+        console.log("Move-se para a horizontal e vertical");
+        break;    
+    case "cavalo":
+        console.log("Move-se duas casas horizontalmente ou verticalmente e então uma casa a mais em um angulo reto");
+        break;
+    case "peão":
+        console.log("Move-se para frente, captura na diagonal");
+        break;
+    default:
+        console.log("Erro! peça inválida.");
+        break;
+}
