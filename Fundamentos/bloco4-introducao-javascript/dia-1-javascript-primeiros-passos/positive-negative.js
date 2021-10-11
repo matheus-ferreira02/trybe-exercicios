206be9036f2823dc8b8a6bac9406ba5b1df95b68
Matheus-Ferreira <m.ferreira.araujo2016@gmail.com>
2021-10-11 16:19:01 -0300

    exercicio de positivo e negativo

---
+++ Fundamentos/bloco4-introducao-javascript/dia-1-javascript-primeiros-passos/positive-negative.js
@@ -0,0 +1,9 @@
+const valor = -10;
+
+if (valor > 0) {
+    console.log("Valor é positivo");
+} else if (valor == 0){
+    console.log("Valor é nulo");
+} else {
+    console.log("Valor é negativo");
+}
