20a58bbd90c3a02c8418976b3b1ea386dd968eed
Matheus-Ferreira <m.ferreira.araujo2016@gmail.com>
2021-10-14 14:53:36 -0300

    adicionei alguns algoritimos

--- Fundamentos/bloco4-introducao-javascript/dia-2--javascript-array-loop-for/exercicio1.js
+++ Fundamentos/bloco4-introducao-javascript/dia-2--javascript-array-loop-for/exercicio1.js
@@ -1,5 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+//algoritimo:
+//- percorrer cada item da lista
+//- mostrar na tela cada item da lista
+
+// criar laço de repetição
+// criar variavel que vai receber cada item
+// mostrar a variavel na tela
+
 for (let number of numbers) {
     console.log(number);
 }
--- Fundamentos/bloco4-introducao-javascript/dia-2--javascript-array-loop-for/exercicio2.js
+++ Fundamentos/bloco4-introducao-javascript/dia-2--javascript-array-loop-for/exercicio2.js
@@ -1,5 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+//algoritimo:
+//- criar variavel que ira somar cada item
+//- criar o laço de repetição
+//- criar a variavel que recebe cada item
+//- percorrer cada item da lista
+//- somar o item na variavel
+//- mostrar na tela a soma dos itens
+
 let sum = 0;
 
 for (let number of numbers) {
