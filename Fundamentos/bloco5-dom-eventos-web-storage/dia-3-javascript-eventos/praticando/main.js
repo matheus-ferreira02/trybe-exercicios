0a9e1d98181ad99e41766b51a96ea591c069ea87
Matheus-Ferreira <m.ferreira.araujo2016@gmail.com>
2021-10-21 18:58:45 -0300

    praaticando concluido

--- Fundamentos/bloco5-dom-eventos-web-storage/dia-3-javascript-eventos/praticando/main.js
+++ Fundamentos/bloco5-dom-eventos-web-storage/dia-3-javascript-eventos/praticando/main.js
@@ -30,12 +30,31 @@ thirdLi.addEventListener('mouseup', removeClass);
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
+input.addEventListener('keyup', function() {
+    texto = document.querySelector('.tech');
+    texto.innerText = input.value;
+})
+
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
 
+myWebpage.addEventListener('dblclick', function(){
+    window.open('https://matheus-ferreira02.github.io/');
+}) 
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+
+myWebpage.addEventListener('mousemove', function(event) {
+    text = event.target;
+    text.style.color = 'yellow';
+});
+
+myWebpage.addEventListener('mouseout', function(event) {
+    text = event.target;
+    text.style.color = '';
+});
 
 // Segue abaixo um exemplo do uso de event.target:
 
