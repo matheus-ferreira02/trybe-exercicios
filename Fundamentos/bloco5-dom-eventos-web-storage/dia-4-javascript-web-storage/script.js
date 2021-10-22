a2b6b29e298d21a3b57408e66f1e3385860e0f0a
Matheus-Ferreira <m.ferreira.araujo2016@gmail.com>
2021-10-22 19:24:02 -0300

    commit

---
+++ Fundamentos/bloco5-dom-eventos-web-storage/dia-4-javascript-web-storage/script.js
@@ -0,0 +1,26 @@
+textBackground = document.getElementById('text-background');
+colorText = document.getElementById('color-text');
+fontSize = document.getElementById('font-size');
+lineHeight = document.getElementById('line-height');
+fontFamily = document.getElementById('font-family');
+console.log(fontFamily.value);
+
+textBackground.addEventListener('keyup', function() {
+
+});
+
+colorText.addEventListener('keyup', function() {
+
+});
+
+fontSize.addEventListener('keyup', function() {
+
+});
+
+lineHeight.addEventListener('keyup', function() {
+
+});
+
+fontFamily.addEventListener('keyup', function() {
+
+});
