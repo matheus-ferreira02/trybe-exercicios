c19f4f80cecef6966dc33bbf6012a827c55b8ca9
Matheus-Ferreira <m.ferreira.araujo2016@gmail.com>
2022-01-12 15:02:47 -0300

    monstrando api

--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/dog_api/src/App.js
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/dog_api/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      dogData: '',
+      render: true,
+    }
+  }
+
+  fetchDog = () => {
+    this.setState({ loading: true }, async () => {
+      const url = 'https://dog.ceo/api/breeds/image/random';
+      const request = await fetch(url);
+      const dogData = await request.json();
+      this.setState({ dogData, loading: false })
+    })
+  }
+
+  componentDidUpdate(__prevProps, prevState) {
+    console.log(prevState)
+  }
+
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  componentWillUnmount(__prevProps, prevState) {
+    console.log(prevState.dogData.message)
+  }
+
+  shouldComponentUpdate(__nextProps, nextState) {
+    const { dogData } = nextState;
+    let render = true;
+    if (dogData.length !== 0) {
+      render = !dogData.message.includes('terrier')
+    }
+
+    return render;
+  }
+
+  showDog = () => {
+    const { dogData } = this.state;
+    return (
+      <div>
+        <img src={ dogData.message } alt="Imagem do cão"/>
+        <button type="button" onClick={ this.fetchDog }>Pŕoximo cachorro</button>
+      </div>
+    )
+  }
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <div className="App">
+       { loading ? 'Loading...' : this.showDog() }
+      </div>
   );
-}
+}}
 
 export default App;
