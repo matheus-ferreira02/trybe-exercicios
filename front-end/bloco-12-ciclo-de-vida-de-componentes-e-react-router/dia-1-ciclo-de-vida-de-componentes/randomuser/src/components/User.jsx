00489bd2d5a57b3a1d78a84a4c93f682c3d99e74
Matheus-Ferreira <m.ferreira.araujo2016@gmail.com>
2022-01-12 12:12:44 -0300

    mostrar api, com loading e renderizar pesoas abaixo de 50 anoss

--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/randomuser/src/components/User.jsx
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/randomuser/src/components/User.jsx
@@ -12,28 +12,47 @@ export default class User extends Component {
     this.fetchApi = this.fetchApi.bind(this);
   }
 
-  async fetchApi() {
-    try{
-      const url = 'https://api.randomuser.me/';
-      const request = await fetch(url);
-      const dataJson = await request.json();
-      const results = await dataJson.results;
-      this.setState({ api: results[0], loading: false })
-    } catch (error) {
-      console.log(error);
-    }    
+  fetchApi() {
+    this.setState({
+      loading: true,
+    }, async () => {
+      try{
+        const url = 'https://api.randomuser.me/';
+        const request = await fetch(url);
+        const dataJson = await request.json();
+        const results = await dataJson.results;
+        this.setState({ api: results[0], loading: false })
+      } catch (error) {
+        console.log(error);
+      } 
+    })     
   }
 
   componentDidMount() {
     this.fetchApi();
   }
 
+  shouldComponentUpdate(__nextProps, nextState) {
+    const { api } = nextState;
+    const renderApi = api.length !== 0 && api.dob.age < 50;
+    console.log(renderApi)
+
+    return renderApi;
+  }
+
   render() {
     const { loading, api } = this.state;
+    const { picture, name, email, dob } = api;
 
     return (
       <div>
-        { loading ? 'loading...' : <p>{ api.gender }</p>}
+        { loading ? 'loading...'
+          : <div>
+              <img src={ picture.large } alt={ name.first }/>
+              <p>{ name.first } { name.last } </p>
+              <p>{ email }</p>
+              <p>{ dob.age }</p>
+            </div> }
       </div>
     )
   }
