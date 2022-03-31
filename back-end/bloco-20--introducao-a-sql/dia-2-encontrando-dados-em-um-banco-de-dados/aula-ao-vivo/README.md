7fcd853a27b3dd3b73f8e7c1b6e97c31223348c1
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-03-31 18:06:09 -0300

    exercicio da aula ao vivo

---
+++ back-end/bloco-20--introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/aula-ao-vivo/README.md
@@ -0,0 +1,11 @@
+- Somente os nomes dos livros cadastrados na biblioteca;
+
+- Quantos livros temos cadastrados em nossa biblioteca;
+
+- Lista com 2 livros e suas informações, mas eu não quero o livro 'Um livro do Ano' e 'Novas conquistas novas responsabilidades';
+
+- Os livros mais recentes primeiro, e também em ordem alfabética;
+
+- Todas as informações do livro com maior estoque na biblioteca. Somente o com o maior estoque;
+
+- Uma lista com os 4 livros mais vendidos. Como poderia encontrar isso?
---
+++ back-end/bloco-20--introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/aula-ao-vivo/aula.sql
@@ -0,0 +1,26 @@
+CREATE database IF NOT EXISTS faculdade;
+
+USE faculdade;
+
+CREATE TABLE IF NOT EXISTS biblioteca (
+    `nome` VARCHAR(40) CHARACTER SET utf8,
+    `emprestado` VARCHAR(3) CHARACTER SET utf8,
+    `quantidade` INT,
+    `ano_lancamento` INT,
+    `vendas` INT
+);
+
+INSERT INTO faculdade.biblioteca VALUES
+    ('Um Livro do Ano','Não',5,2019,350),
+    ('Novas conquistas novas responsabilidades','Sim',10,2020,1412),
+    ('O retorno do Jedi','Não',6,2019,845),
+    ('Café ou Chá, eis a questão','Não',13,2020,1337);
+
+
+
+
+SELECT * FROM biblioteca;
+SELECT 	count(nome) FROM biblioteca;
+SELECT * FROM biblioteca LIMIT 2 OFFSET 2;
+SELECT * FROM biblioteca ORDER BY quantidade DESC LIMIT 1;
+SELECT * FROM biblioteca ORDER BY vendas DESC LIMIT 4;
