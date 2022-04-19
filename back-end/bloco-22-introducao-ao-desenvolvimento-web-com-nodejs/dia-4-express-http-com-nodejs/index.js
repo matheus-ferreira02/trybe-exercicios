954fb3768998dd36b467d06fcd76942fb96a430d
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-04-19 20:44:05 -0300

    exercicios 5, 6, 7

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs').promises;
 const PORT = 3005;
 const app = express();
 
@@ -24,12 +25,37 @@ app.post('/greetings', validationAge, (req, res) => {
   const { name } = req.body;
 
   res.status(200).json({ "message": `Hello, ${name}!` })
-})
+});
 
 app.put('/users/:name/:age', (req, res) => {
   const { name, age } = req.params;
 
   res.status(200).send({ "message": `Seu nome é ${name} e você tem ${age} anos de idade` });
-})
+});
+
+const getData = async () => {
+  try {
+    const data = await fs.readFile('simpsons.json');
+    return JSON.parse(data);
+  } catch {
+    throw new Error('Erro');
+  }  
+}
+
+app.get('/simpsons', async (_req, res) => {
+  const json = await getData();
+  
+  res.status(200).json({ "results": [...json] });
+});
+
+app.get('/simpsons/:id', async (req, res) => {
+  const characters = await getData();
+  const { id } = req.params;
+  const character = characters.find((element) => element.id === id);
+
+  if(!character) return res.status(404).json({ "message": "simpson not found" });
+  
+  res.status(200).json({ "result": character });
+});
 
 app.listen(PORT);
