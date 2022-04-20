3767702f72aab0e4e1df16c4f0b5c0a4da64c484
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-04-19 22:12:50 -0300

    exercicios 8 refatorado

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -33,48 +33,45 @@ app.put('/users/:name/:age', (req, res) => {
   res.status(200).send({ "message": `Seu nome é ${name} e você tem ${age} anos de idade` });
 });
 
-const getData = async () => {
+const getDataMiddleware = async (req, _res, next) => {
   try {
-    const data = await fs.readFile('simpns.json');
-    return JSON.parse(data);
+    const data = await fs.readFile('simpsons.json');
+    req.characters = JSON.parse(data);
+    next();
   } catch {
-    return false;
-  }  
+    return next({ status: 404, message: 'Arquivo não encontrado' });
+  }
 }
 
-app.post('/simpsons/regyster', async (req, res) => {
-  const characters = await getData();
-  if (!characters) return res.status(500).json({ "message": "Internal Server Error" });
+app.get('/simpsons', getDataMiddleware, async (req, res) => {
+  const { characters } = req;
+  res.status(200).json({ "results": [...characters] });
+});
+
+app.post('/simpsons/regyster', getDataMiddleware, async (req, res) => {
+  const { characters } = req;
   const { id, name } = req.body;
 
   const containCharacter = characters.some((character) => character.id === id);
   if(containCharacter) return res.status(409).json({ "message": "id already exists" });
-
   characters.push({ id, name });
   fs.writeFile('simpsons.json', JSON.stringify(characters, null, 2));
   res.status(200).json({ "results": [...characters] });
 });
 
-app.get('/simpsons/:id', async (req, res) => {
-  const characters = await getData();
-  if (!characters) return res.status(500).json({ "message": "Internal Server Error" });
+app.get('/simpsons/:id', getDataMiddleware, async (req, res) => {
+  const { characters } = req;
   const { id } = req.params;
   const character = characters.find((element) => element.id === id);
 
   if(!character) return res.status(404).json({ "message": "simpson not found" });
   
-  res.status(204).end();
+  res.status(200).json({ "result": character });
 });
 
-app.get('/simpsons', async (_req, res) => {
-  const characters = await getData();
-  if (!characters) return res.status(500).json({ "message": "Internal Server Error" });
-  res.status(200).json({ "results": [...characters] });
-});
-
-const handleError = (_err, _req, res, _next) => {
-  console.log('entrou');
-  res.status(500).json({ "message": "Internal Server Error" });
+const handleError = (err, _req, res, _next) => {
+  const { status, message } = err;
+  res.status(status).json({ message });
 }
 
 app.use(handleError);
