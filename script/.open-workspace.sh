236e01c0aa92b84f7fc6b0e291096d7116b88243
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-04 11:08:10 -0300

    guardando script

---
+++ script/.open-workspace.sh
@@ -0,0 +1,4 @@
+nohup google-chrome https://app.betrybe.com/login > .resultado.odt & 
+nohup slack > .resultado.odt & 
+nohup discord > .resultado.odt & 
+nohup flatpak run us.zoom.Zoom > .resultado.odt &
