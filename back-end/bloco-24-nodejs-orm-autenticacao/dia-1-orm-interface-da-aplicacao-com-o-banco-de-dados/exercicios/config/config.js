53766e5b5b09afee30acf664eaeca49d59fc5c9c
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-16 20:44:09 -0300

    feat: adicionando variaveis de ambiente no config.js do sequelize

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios/config/config.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios/config/config.js
@@ -1,9 +1,11 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.HOSTNAME,
     "dialect": "mysql"
   },
   "test": {
